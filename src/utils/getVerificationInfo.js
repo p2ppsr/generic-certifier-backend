ad52cf323ab53e9f225cd1c06c5746ab2f03313f
Brayden Langley <brayden167@gmail.com>
2023-12-27 15:46:06 -0800

    New deps

--- src/utils/getVerificationInfo.js
+++ src/utils/getVerificationInfo.js
@@ -1,5 +1,6 @@
-// TODO: Consider rename
+// Helper function for retrieving stored verification data
 const getVerificationInfo = async (verificationId) => {
   // Validate the verification info with external API as needed
+  return true
 }
 module.exports = { getVerificationInfo }
