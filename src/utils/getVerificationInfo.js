e205ab2711cac708a98573c92fc76cfc3703e732
Tone Engel <tone@kizmet.org>
2025-01-27 13:57:56 -0500

    bring over old javascript, beging rework to typescript / wallet-toolbox

---
+++ src/utils/getVerificationInfo.js
@@ -0,0 +1,6 @@
+// Helper function for retrieving stored verification data
+const getVerificationInfo = async (verificationId) => {
+  // Validate the verification info with external API as needed
+  return true
+}
+module.exports = { getVerificationInfo }
